4330695022caf0d9e7f4a58ce7010516a5b0e7b8
Alexandra Ballester <alexandramballester@gmail.com>
2019-10-10 14:13:48 -0700

    adds react app component, webpack dev server also added but not being used right now. Client is erved through express server. Moved html file into dist, added script tag for bundle file."

--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const bodyParser = require('body-parser');
 
-app.use(express.static(`${__dirname}../client/dist`));
-app.use(express.static(`${__dirname}../client/index`));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
+
 app.listen(3000, function() {
   console.log('listening on port 3000');
-});
-
-app.get('/test', (req, res) => {
-  res.send('success');
 });
