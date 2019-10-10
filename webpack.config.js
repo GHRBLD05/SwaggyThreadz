5452201953e167d862020c2d9d086a6abfd9310b
Evan Schwarz <evanschwarz90@gmail.com>
2019-10-09 19:58:57 -0600

    configures webpack and babel and sets up a couple build scripts on the package.json

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+  },
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
