2a186ca03c0dedef39256b4494bbf7b76169336a
Alexandra Ballester <alexandramballester@gmail.com>
2019-10-11 15:34:50 -0700

    resolved conflicts

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import QuestionsModule from './Questions_answers/QuestionsModule.jsx';
 
@@ -12,3 +13,19 @@ export default class App extends React.Component {
         )
     }
 }
+=======
+import React from 'react';
+import QuestionsModule from './Questions_answers/QuestionsModule.jsx';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-8">test</div>
+        <div className="col-md-4">othertest</div>
+        <div className="col-md-12">thirdtest</div>
+      </div>
+    );
+  }
+}
+>>>>>>> Stashed changes
