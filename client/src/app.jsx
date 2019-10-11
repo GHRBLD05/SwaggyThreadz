acba024b39d020541e5b3a3f4da1ad4648116764
Greg Smith <54219569+gregsmithrepo@users.noreply.github.com>
2019-10-11 14:37:16 -0600

    bootstrap in place, still needs finagling

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -4,10 +4,10 @@ import QuestionsModule from './Questions_answers/QuestionsModule.jsx';
 export default class App extends React.Component {
     render() {
         return (
-            <div class="row">
-                <div class="col-md-8">test</div>
-                <div class="col-md-4">othertest</div>
-                <div class="col-md-12">thirdtest</div>
+            <div className="row">
+                <div className="col-md-8">test</div>
+                <div className="col-md-4">othertest</div>
+                <div className="col-md-12">thirdtest</div>
             </div>
         )
     }
