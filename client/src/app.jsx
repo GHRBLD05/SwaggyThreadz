2f565b51b497054e9969c0bf2d24efca0be3e8b3
Axel <axelbrostedt@gmail.com>
2019-10-11 17:01:41 -0600

    Adds basic styling to my search bar and some functionality. The state is now updated to whatever the user types and there is also an image of a magnifying glass in the right corner

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import QuestionsModule from './Questions_answers/QuestionsModule.jsx';
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col-md-8">test</div>
-                <div className="col-md-4">othertest</div>
-                <div className="col-md-12">thirdtest</div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-8">test</div>
+        <div className="col-md-4">othertest</div>
+        <div className="col-md-12">thirdtest</div>
+        <QuestionsModule />
+      </div>
+    );
+  }
 }
