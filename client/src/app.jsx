51506e46cace3f0d93a88be04dd8b5848d300049
Greg Smith <54219569+gregsmithrepo@users.noreply.github.com>
2019-10-10 18:21:56 -0600

    adding missing files

---
+++ client/src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>test</h1>
+            </div>
+        )
+    }
+}
