4caf6626aaeac7725ad0df6fcb33a3cb4d1a285d
Jae Han <JaeHan@Jaes-MacBook-Air.local>
2019-10-11 19:16:43 -0600

    made 3 total stars showing full, empty, half filled working on logic next.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import QuestionsModule from './Questions_answers/QuestionsModule.jsx';
+import React from "react";
+import QuestionsModule from "./Questions_answers/QuestionsModule.jsx";
+import Star from "/Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/Star.jsx";
+import Emptystar from "/Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/Emptystar.jsx";
+import Halfstar from "//Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/Halfstar.jsx";
 
 export default class App extends React.Component {
   render() {
@@ -7,6 +10,9 @@ export default class App extends React.Component {
       <div>
         <h1>test</h1>
         <QuestionsModule />
+        <Star />
+        <Emptystar />
+        <Halfstar />
       </div>
     );
   }
