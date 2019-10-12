2d2a507b510c1a29eb17c236d03531e6cddc63b9
Axel <axelbrostedt@gmail.com>
2019-10-12 11:21:21 -0600

    Took out Jae's imports temporarily since it wouldn't let me render anything with them in the file

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import QuestionsModule from "./Questions_answers/QuestionsModule.jsx";
-import Star from "/Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/Star.jsx";
-import Emptystar from "/Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/Emptystar.jsx";
-import Halfstar from "//Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/Halfstar.jsx";
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>test</h1>
-        <QuestionsModule />
-        <Star />
-        <Emptystar />
-        <Halfstar />
-      </div>
-    );
-  }
-}
-=======
 import React from 'react';
 import QuestionsModule from './Questions_answers/QuestionsModule.jsx';
 
@@ -33,4 +12,3 @@ export default class App extends React.Component {
     );
   }
 }
->>>>>>> master
