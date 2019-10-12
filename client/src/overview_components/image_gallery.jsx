d5d39e9d7d29f677fe7e244661ed9aa5a427a83a
Alexandra Ballester <alexandramballester@gmail.com>
2019-10-11 21:53:50 -0700

    implemented nav bar, added a second row to the app.jsx so i could see a multiple modules on the page. also made some changesto the image gallery..have a basic carousel up

--- client/src/overview_components/image_gallery.jsx
+++ client/src/overview_components/image_gallery.jsx
@@ -1,11 +1,75 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class ImageGallery extends Component {
   render() {
     return (
       <div>
-        <p>this will be the main photo</p>
+        <div
+          id="carouselExampleIndicators"
+          className="carousel slide"
+          data-ride="carousel"
+        >
+          <ol className="carousel-indicators">
+            <li
+              data-target="#carouselExampleIndicators"
+              data-slide-to="0"
+              className="active"
+            ></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+                alt="first slide"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+                alt="Second slide"
+              />
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src="..." alt="Third slide" />
+            </div>
+          </div>
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
       </div>
     );
   }
 }
+// {
+//   /* <img
+// src="https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+// className="img-fluid"
+// alt="Responsive"
+// /> */
+// }
