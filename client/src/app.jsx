b74ce7a0420db356b1e477a25d4239a6597777a1
Alexandra Ballester <alexandramballester@gmail.com>
2019-10-11 21:56:20 -0700

    moved the styles folder to the dist

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,31 +1,17 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import QuestionsModule from './Questions_answers/QuestionsModule.jsx';
-
-export default class App extends React.Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col-md-8">test</div>
-                <div className="col-md-4">othertest</div>
-                <div className="col-md-12">thirdtest</div>
-            </div>
-        )
-    }
-}
-=======
 import React from 'react';
 import QuestionsModule from './Questions_answers/QuestionsModule.jsx';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-md-8">test</div>
-        <div className="col-md-4">othertest</div>
-        <div className="col-md-12">thirdtest</div>
+      <div className="container-fluid maincontainer">
+        <div className="row">
+          {/* <div className="col-md-8">test</div>
+          <div className="col-md-2">othertest</div>
+          <div className="col-md-2">thirdtest</div> */}
+        </div>
+        <div className="row"></div>
       </div>
     );
   }
 }
->>>>>>> Stashed changes
