ff66a5294e3c0077068436fb1d62154c895b09a6
Evan Schwarz <evanschwarz90@gmail.com>
2019-10-14 17:17:27 -0600

    very basic skeleton for related products and outfit

---
+++ client/src/relatedAndOutfit/relatedProduct.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class RelatedProduct extends React.Component {
+  render() {
+    return (
+      <div>
+        <img
+          src="https://scene7.zumiez.com/is/image/zumiez/pdp_hero/Champion-Men-s-Rally-Pro-Black-%26-White-Shoes-_298256.jpg"
+          alt="shoe"
+        ></img>
+      </div>
+    );
+  }
+}
