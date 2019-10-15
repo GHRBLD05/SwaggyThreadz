fe55a52517245df3e9d973e67bccdd73abeb2a56
Jae Han <JaeHan@Jaes-MacBook-Air.local>
2019-10-15 11:02:55 -0600

    testing logic for star

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 import React from "react";
 import QuestionsModule from "./Questions_answers/QuestionsModule.jsx";
-import Star from "/Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/Star.jsx";
-import Emptystar from "/Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/Emptystar.jsx";
-import Halfstar from "//Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/Halfstar.jsx";
+import StarRatingComponent from "/Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/StarRatingComponent.jsx";
+// import Emptystar from '/Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/Emptystar.jsx';
+// import Halfstar from '//Users/JaeHan/Galvanize/Retail-Product-Webpage/client/src/star_component/Halfstar.jsx';
 
 export default class App extends React.Component {
   render() {
@@ -11,27 +10,10 @@ export default class App extends React.Component {
       <div>
         <h1>test</h1>
         <QuestionsModule />
-        <Star />
-        <Emptystar />
-        <Halfstar />
+        <StarRatingComponent />
+        {/* <Emptystar />
+        <Halfstar /> */}
       </div>
     );
   }
 }
-=======
-import React from 'react';
-import QuestionsModule from './Questions_answers/QuestionsModule.jsx';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="row">
-        <div className="col-md-8">test</div>
-        <div className="col-md-4">othertest</div>
-        <div className="col-md-12">thirdtest</div>
-        <QuestionsModule />
-      </div>
-    );
-  }
-}
->>>>>>> master
