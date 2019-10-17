73c39aef9c881a984723dcb000674a8aa1d086ee
Greg Smith <54219569+gregsmithrepo@users.noreply.github.com>
2019-10-17 14:17:14 -0600

    reduced and full review list state transition on load more reviews

--- client/src/reviewcomponent/controls.jsx
+++ client/src/reviewcomponent/controls.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
 export default class Controls extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+        this.loadMoreReviews = new CustomEvent("loadMoreReviews", null)
+    }
+
+    onLoadMoreReviews(e) {
+        document.getElementById('reviews').dispatchEvent(this.loadMoreReviews);
+    }
+
     render() {
       return  (
-            <div id="controls">
-                <h1>Controls</h1>
-            </div>
+          <div id="controls">
+              <button id="morereviewsbutton" type="button" onClick={this.onLoadMoreReviews.bind(this)}>MORE REVIEWS</button>
+              <button id="addreviewbutton" type="button">ADD A REVIEW</button>
+          </div>
         )
     }
 }
--- client/src/reviewcomponent/reviews.jsx
+++ client/src/reviewcomponent/reviews.jsx
@@ -8,9 +8,10 @@ export default class Reviews extends React.Component {
     constructor() {
         super();
 
-        this.url = "http://52.26.193.201:3000/reviews/2/list"
+        this.url = "http://52.26.193.201:3000/reviews/2/list"; //TODO: Add params to load different items
         this.views = ["REDUCED", "FULL"];
         this.state = {
+            viewCount: 2,
             viewState: this.views[0],
             reviews: {
                 product: undefined,
@@ -22,9 +23,13 @@ export default class Reviews extends React.Component {
     }
 
     componentDidMount() {
-        document.getElementById('reviews').addEventListener('sortingChanged', e => this.getReviews(e.detail.sortOptions));
-
         this.getReviews('relevance');
+
+        document.getElementById('reviews').addEventListener('sortingChanged', e => this.getReviews(e.detail.sortOptions));
+        document.getElementById('reviews').addEventListener('loadMoreReviews', e => {
+            this.setState({ viewCount: this.state.viewCount += 2 });
+            this.state.viewCount >= this.state.reviews.results.length ? this.setState({ viewState: this.views[1] }) : this.setState();
+        });
     }
 
     getReviews(sortOptions) {
@@ -40,31 +45,39 @@ export default class Reviews extends React.Component {
     }
 
     render() {
-        if (this.state.reviews.product === undefined) {
+        if (this.state.reviews.product === undefined) /* INIT */ {
             return (
                 <div id="reviews" className="col-md-8 row-">
                     {<Filter />}
-                    {<Controls />}
+                    {<Controls reviewsState={this.state.viewState}/>}
                 </div>
             )
         }
-        else if (this.state.viewState === this.views[0]) {
+        else if (this.state.viewState === this.views[0]) /* REDUCED */ {
             console.log("STATE:", this.state.viewState);
+
+            var reviews = [];
+            let i = 0
+            for (; i < this.state.viewCount && i < this.state.reviews.count; i++) {
+                reviews.push(<Review key={this.state.reviews.results[i].review_id} review={this.state.reviews.results[i]} />);
+            }
+
+            return (
+                <div id="reviews" className="col-md-8 row-">
+                    {<Filter />}
+                    {reviews}
+                    {<Controls reviewsState={this.state.viewState}/>}
+                </div>
+            )
+        }
+        else if (this.state.viewState === this.views[1]) /* FULL */ {
+            console.log("STATE:", this.state.viewState);
+
             return (
                 <div id="reviews" className="col-md-8 row-">
                     {<Filter />}
                     {this.state.reviews.results.map((review) => <Review key={review.review_id} review={review} />)}
-                    {<Controls />}
-                </div>
-            )
-        }
-        else if (this.state.viewState === this.views[1]) {
-            console.log("STATE:", this.state.viewState);
-            return (
-                <div id="reviews" className="col-md-8 row-">
-                    {<Filter />}
-                    {this.state.reviews.results.map((review) => <Review review={review} />)}
-                    {<Controls />}
+                    {<Controls reveiwsState={this.state.viewState}/>}
                 </div>
             )
         }
