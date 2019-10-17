7a2a29a03df00a77bb23fd4f0ee4d2b052fc7b13
Greg Smith <54219569+gregsmithrepo@users.noreply.github.com>
2019-10-17 12:59:18 -0600

    filter options populate appropriate reveiws

--- client/src/reviewcomponent/filter.jsx
+++ client/src/reviewcomponent/filter.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
 export default class Filter extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.sortingChanged = new CustomEvent("sortingChanged", { detail: { sortOptions: null } })
+    }
+
+    onSortingChanged(e) {
+        this.sortingChanged.detail.sortOptions = e.target.value;
+        document.getElementById("reviews").dispatchEvent(this.sortingChanged);
+    }
+
     render() {
       return  (
           <div id="rnrFilter">
               <div className="focus filter">250 reviews, sorted by
-                  <select className="focus dropdown">
+                  <select onChange={this.onSortingChanged.bind(this)} className="focus dropdown">
+                      <option value="relevance">relevance</option>
                       <option value="helpful">helpful</option>
                       <option value="newest">newest</option>
-                      <option value="relevant">relevance</option>
                   </select>
               </div>
             </div>
