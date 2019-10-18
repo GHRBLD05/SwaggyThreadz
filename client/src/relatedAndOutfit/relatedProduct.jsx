7e3328f0d1f408f48f03eb64a02f74dba7f12ff9
Evan Schwarz <evanschwarz90@gmail.com>
2019-10-18 14:11:27 -0600

    conforms size of images in related products slider

--- client/src/relatedAndOutfit/relatedProduct.jsx
+++ client/src/relatedAndOutfit/relatedProduct.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import $ from 'jquery';
 
 const RelatedProduct = props => (
-  <div className="img-fluid mx-auto d-block">
-    <img src={props.product.thumbnail_url} alt={props.product.name} />
+  <div
+    className="img-fluid mx-auto d-block"
+    style={{
+      backgroundColor: 'eee',
+    }}
+  >
+    <img
+      src={props.product.thumbnail_url}
+      alt={props.product.name}
+      style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+    />
   </div>
 );
 
