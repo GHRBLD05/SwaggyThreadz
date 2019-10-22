93ff507d34a98cb3a233419936aec5feba2a24d6
Evan Schwarz <evanschwarz90@gmail.com>
2019-10-21 19:27:45 -0600

    stops new product from being clicked when using comparison modal

--- client/src/relatedAndOutfit/relatedProduct.jsx
+++ client/src/relatedAndOutfit/relatedProduct.jsx
@@ -2,50 +2,53 @@ import React from 'react';
 import RelatedModal from './relatedModal.jsx';
 
 const RelatedProduct = props => (
-  <div
-    onClick={e => {
-      props.handleProductClick(props.product.name);
-    }}
-    onKeyPress={e => {
-      props.handleProductClick(props.product.name);
-    }}
-    tabIndex={0}
-    role="button"
-    className="img-fluid mx-auto d-block"
-    style={{
-      position: 'relative',
-      width: '100%',
-      height: '300px',
-      border: 'red solid 2px',
-    }}
-  >
-    <img
-      src={props.product.thumbnail_url}
-      alt={props.product.name}
-      style={{ width: '100%', height: '200px', objectFit: 'cover' }}
-    />
-    <button
-      type="button"
-      className="relatedModalButton fa fa-star fa-lg"
-      data-toggle="modal"
-      data-target={`#relatedModal${props.product.id}`}
+  <div>
+    <div
       onClick={e => {
-        e.stopPropagation();
+        props.handleProductClick(props.product.name);
       }}
-    ></button>
-    <RelatedModal
-      relatedProduct={props.product}
-      currentProduct={props.currentProduct}
-    />
-    <div className="relatedCardProductInfo">
-      <p className="relatedProductCategory">{props.product.category}</p>
-      <p className="relatedProductName">{props.product.name}</p>
-      <p className="relatedProductPrice">${props.product.default_price}</p>
-      <p className="relatedProductRating">
-        {props.product.averageRating
-          ? `${props.product.averageRating} / 5`
-          : 'No Ratings'}
-      </p>
+      onKeyPress={e => {
+        props.handleProductClick(props.product.name);
+      }}
+      tabIndex={0}
+      role="button"
+      className="img-fluid mx-auto d-block"
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '300px',
+        border: 'red solid 2px',
+      }}
+    >
+      <img
+        src={props.product.thumbnail_url}
+        alt={props.product.name}
+        style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+      />
+      <button
+        type="button"
+        className="relatedModalButton fa fa-star fa-lg"
+        onClick={function(e) {
+          e.preventDefault();
+          e.stopPropagation();
+        }}
+        data-toggle="modal"
+        data-target={`#relatedModal${props.product.id}`}
+      ></button>
+      <RelatedModal
+        relatedProduct={props.product}
+        currentProduct={props.currentProduct}
+      />
+      <div className="relatedCardProductInfo">
+        <p className="relatedProductCategory">{props.product.category}</p>
+        <p className="relatedProductName">{props.product.name}</p>
+        <p className="relatedProductPrice">${props.product.default_price}</p>
+        <p className="relatedProductRating">
+          {props.product.averageRating
+            ? `${props.product.averageRating} / 5`
+            : 'No Ratings'}
+        </p>
+      </div>
     </div>
   </div>
 );
