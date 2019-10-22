2f1f0c3239ac5c6ff44ffe141341d3feb1af0beb
Evan Schwarz <evanschwarz90@gmail.com>
2019-10-21 19:40:33 -0600

    adds hover effect on related product cards

--- client/src/relatedAndOutfit/relatedProduct.jsx
+++ client/src/relatedAndOutfit/relatedProduct.jsx
@@ -2,53 +2,51 @@ import React from 'react';
 import RelatedModal from './relatedModal.jsx';
 
 const RelatedProduct = props => (
-  <div>
-    <div
-      onClick={e => {
-        props.handleProductClick(props.product.name);
+  <div
+    onClick={e => {
+      props.handleProductClick(props.product.name);
+    }}
+    onKeyPress={e => {
+      props.handleProductClick(props.product.name);
+    }}
+    tabIndex={0}
+    role="button"
+    className="img-fluid mx-auto d-block relatedCard"
+    style={{
+      position: 'relative',
+      width: '100%',
+      height: '300px',
+      border: 'red solid 2px',
+    }}
+  >
+    <img
+      src={props.product.thumbnail_url}
+      alt={props.product.name}
+      style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+    />
+    <button
+      type="button"
+      className="relatedModalButton fa fa-star fa-lg"
+      onClick={function(e) {
+        e.preventDefault();
+        e.stopPropagation();
       }}
-      onKeyPress={e => {
-        props.handleProductClick(props.product.name);
-      }}
-      tabIndex={0}
-      role="button"
-      className="img-fluid mx-auto d-block"
-      style={{
-        position: 'relative',
-        width: '100%',
-        height: '300px',
-        border: 'red solid 2px',
-      }}
-    >
-      <img
-        src={props.product.thumbnail_url}
-        alt={props.product.name}
-        style={{ width: '100%', height: '200px', objectFit: 'cover' }}
-      />
-      <button
-        type="button"
-        className="relatedModalButton fa fa-star fa-lg"
-        onClick={function(e) {
-          e.preventDefault();
-          e.stopPropagation();
-        }}
-        data-toggle="modal"
-        data-target={`#relatedModal${props.product.id}`}
-      ></button>
-      <RelatedModal
-        relatedProduct={props.product}
-        currentProduct={props.currentProduct}
-      />
-      <div className="relatedCardProductInfo">
-        <p className="relatedProductCategory">{props.product.category}</p>
-        <p className="relatedProductName">{props.product.name}</p>
-        <p className="relatedProductPrice">${props.product.default_price}</p>
-        <p className="relatedProductRating">
-          {props.product.averageRating
-            ? `${props.product.averageRating} / 5`
-            : 'No Ratings'}
-        </p>
-      </div>
+      data-toggle="modal"
+      data-target={`#relatedModal${props.product.id}`}
+    ></button>
+    <RelatedModal
+      relatedProduct={props.product}
+      currentProduct={props.currentProduct}
+    />
+    <div className="relatedCardProductInfo">
+      <p className="relatedProductCategory">{props.product.category}</p>
+      <p className="relatedProductName">{props.product.name}</p>
+      <p className="relatedProductPrice">${props.product.default_price}</p>
+      <p className="relatedProductRating">
+        {props.product.averageRating
+          ? `${props.product.averageRating} / 5`
+          : 'No Ratings'}
+      </p>
     </div>
   </div>
 );
