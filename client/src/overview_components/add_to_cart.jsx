bc0a130afcf4153f5c7d7010f703d7cb6c399f94
Alexandra Ballester <alexandramballester@gmail.com>
2019-10-26 12:19:34 -0700

    dropdowns work as needed but styling isnt ideal

--- client/src/overview_components/add_to_cart.jsx
+++ client/src/overview_components/add_to_cart.jsx
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class AddToCart extends Component {
-  render() {
-    const sizes = Object.keys(this.props.currentStyle.skus);
-    const qty = Object.values(this.props.currentStyle.skus);
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedSize: ""
+    };
+  }
 
+  handleSizeClick(e) {
+    this.setState({ selectedSize: e.target.value });
+    console.log(this.state.selectedSize);
+  }
+
+  render() {
+    console.log("mydata");
+    console.log(this.state.selectedSize);
+    console.log(this.props.currentStyle.skus);
+    console.log(this.props.currentStyle.skus[this.state.selectedSize]);
+    const sizes = Object.keys(this.props.currentStyle.skus);
+    console.log(this.props.currentStyle.skus);
+
+    const qty = Object.values(this.props.currentStyle.skus);
     return (
       <div className="row">
         <div className="col-md-8">
-          <select className="button focus">
+          <select
+            className="button focus"
+            onChange={e => this.setState({ selectedSize: e.target.value })}
+          >
             {sizes.map((size, i) => (
               <option>{size}</option>
             ))}
@@ -16,8 +36,14 @@ export default class AddToCart extends Component {
         </div>
         <div className="col-md-4">
           <select>
-            {qty.map((size, i) => (
-              <option className="button focus">{size}</option>
+            {Array.from(
+              Array(
+                this.props.currentStyle.skus[this.state.selectedSize]
+              ).keys()
+            ).map(i => (
+              <option className="button focus" type="number" min="1">
+                {i + 1}
+              </option>
             ))}
           </select>
         </div>
